99720529356ba7c66fa9a6ce554ac0b4c0b1342a
komal <komalsuthar151@gmail.com>
2021-01-23 13:57:39 -0500

    corrections

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -10,18 +10,18 @@ class Landing extends Component {
           <Cell col={8}>
             <div >
               <br/><h3 style={{color:"#49abd1"}}>Hello, I am Komal<br/>Welcome to my Website</h3>
-              <p style={{margin:'auto', padding:'5px', fontFamily:'Verdana, sans-serif'}}>I am a passionate web developer. Checkout my work. Feel free to sent a message or just 'Hi'. I would love to hear your feedback.</p>
+              <p style={{margin:'auto', padding:'5px', fontFamily:'Verdana, sans-serif'}}>I am a passionate web developer. Checkout my work. Feel free to send a message or just 'Hi'. I would love to hear your feedback.</p>
               <h4 style={{color:"#49abd1"}}>About Me</h4>
               <p style={{margin:'auto', fontFamily:'Verdana, sans-serif'}}>Software Engineer | MS in Information Systems, Boston, MA <br/>Ambitious Software Engineer. I like building websites with great user experience, travelling, listening to music. I am an adventurous person. Let's find out more...</p>
               </div>
               </Cell>
               
-              <Cell col={4}>
+              <Cell className="avatar" col={4}>
               <img src="https://lh3.googleusercontent.com/G2Sh3ReoAOadqTIfw6iC-JIjn68cAH2DmRFhSL6wst6cLE1Tsu01J9BcKCrqMcCFVk6KlbUO-GgjE-wOUjQAp3xKkHM-PQB1asr80uyiML1cTiRDs9XQr6BQ-v9eI8hBoz7B_L0tihU=w398-h315-p-k" alt="" /> 
             </Cell>
             
 
-            <p class="buttons center">
+            <p className="buttons center">
               <a href="/resume"  class="btn btn-secondary js-scroll-trigger">Resume</a>
               <a href="/projects" class="btn btn-secondary js-scroll-trigger">Projects</a>
               <a href="/contact" class="btn btn-secondary js-scroll-trigger">Contact me</a>
